58e91585b169b7be5bc2094856d9fde22aeb83b3
mobangning <143095748+mobangning@users.noreply.github.com>
2026-04-03 07:45:34 +0800

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include "zygisk.hpp"
 
-// 日志前缀改成了通用的 Global-CamHook
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "Global-CamHook", __VA_ARGS__)
 
 using zygisk::Api;
@@ -18,10 +17,11 @@ using zygisk::ServerSpecializeArgs;
 int (*orig_openat)(int dirfd, const char *pathname, int flags, mode_t mode);
 int (*orig_ioctl)(int fd, unsigned long request, ...);
 
-// 默认节点
+// 【核心变量区】这里绝对不能漏复制，它是配置文件的灵魂！
 char target_node[64] = "/dev/video1"; 
 char fake_node[64] = "/dev/video0";   
 
+// 读取外部配置的逻辑
 void load_config() {
     FILE *fp = fopen("/data/local/tmp/camera_config.txt", "r");
     if (fp) {
@@ -33,7 +33,7 @@ void load_config() {
 // 核心战术 1：全局改门牌
 int my_openat(int dirfd, const char *pathname, int flags, mode_t mode) {
     if (pathname != nullptr && strstr(pathname, target_node)) {
-        LOGD("【全局拦截】成功捕获节点读取 %s，强行重定向至 %s", pathname, fake_node);
+        LOGD("【全局拦截】成功捕获节点读取，强行重定向");
         return orig_openat(dirfd, fake_node, flags, mode);
     }
     return orig_openat(dirfd, pathname, flags, mode);
@@ -46,14 +46,14 @@ int my_ioctl(int fd, unsigned long request, void* argp) {
         struct v4l2_capability *cap = (struct v4l2_capability *)argp;
         if (strstr((char*)cap->card, "Dummy")) {
             LOGD("【全局伪装】成功篡改底层特征为高通 Qcamera2");
-            strlcpy((char*)cap->card, "Qcamera2", sizeof(cap->card));
-            strlcpy((char*)cap->driver, "msm_v4l2", sizeof(cap->driver));
+            // 换用绝对安全标准的 strncpy 防止 NDK 报错
+            strncpy((char*)cap->card, "Qcamera2", sizeof(cap->card) - 1);
+            strncpy((char*)cap->driver, "msm_v4l2", sizeof(cap->driver) - 1);
         }
     }
     return ret;
 }
 
-// 模块类名已重构为通用类名
 class GlobalCameraHookModule : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
@@ -61,15 +61,13 @@ public:
         this->env = env;
     }
 
-    // 注入系统的底层框架服务
     void preServerSpecialize(ServerSpecializeArgs *args) override {
         api->pltHook("libc.so", "openat", (void *)my_openat, (void **)&orig_openat);
         api->pltHook("libc.so", "ioctl", (void *)my_ioctl, (void **)&orig_ioctl);
     }
 
-    // 无差别注入所有被 Zygote 孵化的 App（系统相机、所有第三方应用）
     void preAppSpecialize(AppSpecializeArgs *args) override {
-        load_config(); // 注入时加载一次路径配置
+        load_config(); 
         api->pltHook("libc.so", "openat", (void *)my_openat, (void **)&orig_openat);
         api->pltHook("libc.so", "ioctl", (void *)my_ioctl, (void **)&orig_ioctl);
     }
@@ -79,5 +77,4 @@ private:
     JNIEnv *env;
 };
 
-// 注册激活全局模块
 REGISTER_ZYGISK_MODULE(GlobalCameraHookModule)
